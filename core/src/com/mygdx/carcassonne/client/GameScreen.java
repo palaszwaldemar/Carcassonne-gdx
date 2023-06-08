cf89a8275aaf0d962f8e8fc43913da6c5fe20d81
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-06-08 10:44:28 +0200

    refactor: resizing the window does not affect the size of the tiles

--- core/src/com/mygdx/carcassonne/client/GameScreen.java
+++ core/src/com/mygdx/carcassonne/client/GameScreen.java
@@ -50,7 +50,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
