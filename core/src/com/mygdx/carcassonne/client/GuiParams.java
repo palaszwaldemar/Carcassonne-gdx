dc6f9b63cc7188e5a7e3a600e37609de85afb8d7
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-07-23 15:10:48 +0200

    refactor: - removed 'margin'

--- core/src/com/mygdx/carcassonne/client/GuiParams.java
+++ core/src/com/mygdx/carcassonne/client/GuiParams.java
@@ -4,5 +4,4 @@ public class GuiParams {
     public static final int WIDTH = 1800;
     public static final int HEIGHT = 900;
     public static final int TILE_SIZE = 100;
-//    public static final int MARGIN = 10;
 }
